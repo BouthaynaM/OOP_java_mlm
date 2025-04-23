3a6cd84e274d8ad57c5be81c3e49cff14e21db9e
BouthaynaM <158314753+BouthaynaM@users.noreply.github.com>
2025-04-23 22:11:37 +0100

    Update NaiveBayesClassifier.java

--- NaiveBayesClassifier.java
+++ NaiveBayesClassifier.java
@@ -5,5 +5,16 @@ import java.util.Map;
 
 public class NaiveBayesClassifier 
 {
-    
+    //this private variable is going to hold my frequency table object, so the classifier has a frequency table
+    private FrequencyTable frequencyTable;
+
+    //this will be the default constructor
+    public NaiveBayesClassifier() 
+    {
+        //creating a new frequency table object
+        this.frequencyTable = new FrequencyTable();
+        //calling the loadpredifinedcounts method on this table, which will populate the table with my hard coded values
+        this.frequencyTable.loadPredefinedCounts();
+    }
+
 }
