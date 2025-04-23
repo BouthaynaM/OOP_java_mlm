8bc35bd59786684c20c33bb082ffc27a5d65c9be
BouthaynaM <158314753+BouthaynaM@users.noreply.github.com>
2025-04-23 23:50:55 +0100

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,8 @@
+//name: Bouthayna Metarfi
+//purpose: will control the main flow 
+public class Controller {
+
+    //setting the default file path as static
+    private static final String DEFAULT_FILE_PATH = "PersonHasFever.csv";
+
+}
