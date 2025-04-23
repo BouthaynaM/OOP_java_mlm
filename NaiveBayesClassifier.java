36867828cc15a872ee48471ade8d68513422e4ae
BouthaynaM <158314753+BouthaynaM@users.noreply.github.com>
2025-04-23 12:49:01 +0100

    Add files via upload

---
+++ NaiveBayesClassifier.java
@@ -0,0 +1,9 @@
+//name: Bouthayna Metarfi
+//purpose: this will be the algorithm for the predictions
+import java.util.HashMap;
+import java.util.Map;
+
+public class NaiveBayesClassifier 
+{
+    
+}
