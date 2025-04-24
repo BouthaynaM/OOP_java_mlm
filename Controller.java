b962594a2d579e9ade778b50e981e0d71ef799a5
BouthaynaM <158314753+BouthaynaM@users.noreply.github.com>
2025-04-24 17:17:32 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,8 +1,35 @@
 //name: Bouthayna Metarfi
-//purpose: will control the main flow 
-public class Controller {
+//purpose: controlling the flow of the applicatin, connecting my model with the view
 
-    //setting the default file path as static
+import java.util.Map;
+
+public class Controller 
+{
+    //referencing my prediction algorithm, the datamanger and the mainview to interact with
+    private NaiveBayesClassifier classifier;
+    private DataManager dataManager;
+    private MainView mainView;
+    
+    //a constant with the value PersonHasFver.csv, the data that the model will use
     private static final String DEFAULT_FILE_PATH = "PersonHasFever.csv";
+    
+    //controller constructor
+    public Controller() 
+    {
+        //creating a new naive bayes classifier instance, which will be populated with predefined data
+        classifier = new NaiveBayesClassifier();
+        
+        //creating a new data manager with the default file path
+        dataManager = new DataManager(DEFAULT_FILE_PATH);
+        
+        //initialisng the view to null, because it will be set later with the setmainview method
+        mainView = null;
+    }
+    
+    //this method will let me set the mainview reference after the controller is created
+    public void setMainView(MainView mainView) 
+    {
+        this.mainView = mainView;
+    }
 
 }
